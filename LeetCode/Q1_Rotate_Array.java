e9d3f17cbea56ca51b7d2a3e591ee31062b2a6e3
yashchaudhary1605 <chaudharyyash64036@gmail.com>
2026-04-21 14:35:01 +0530

    [Yash Chaudhary] added Q1_Rotate_Array

--- LeetCode/Q1_Rotate_Array.java
+++ LeetCode/Q1_Rotate_Array.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n= nums.length;
+        int[] arr = new int[n];
+        k=k%n;
+        int x=0;
+        for(int i=k;i<n;i++){
+            arr[i]=nums[x];
+            x++;
+        }
+          
+        for(int i=0;i<k;i++){
+            arr[i]=nums[x];
+            x++;
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=arr[i];
+        }
+    }
+}
